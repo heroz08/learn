a2566864a236b07860ac240106ecefaf1d113dd7
heroz08 <zilongerdai@gmail.com>
2022-01-24 10:42:36 +0800

    数据结构

--- go/project1/helloworld.go
+++ go/project1/helloworld.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+var (
+	n = 2
+	c = "hello"
+)
+
 func main() {
 	fmt.Println("hello world")
 	age()
@@ -9,5 +14,16 @@ func main() {
 
 func age() {
 	var a = 21
-	fmt.Println("age",a)
+	fmt.Println("age", a)
+	var n1, n2, n3 = 1, '2', 2.8
+	sex:="男"
+	fmt.Println(n1, n2, n3, n, c, sex)
 }
+
+// var n1,n2,n3 = 1, '2',2.8
+
+// func log(){
+// 	fmt.Println(v)
+// }
+
+// log(n1)
